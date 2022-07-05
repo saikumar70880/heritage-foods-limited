d1edd28f5e9271ac1ccebe4138d009affb118ba8
Saikumar Aleti <Saikumar.Aleti@lntinfotech.com>
2022-07-06 00:18:35 +0530

    Third Commit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -11,7 +11,7 @@ const getSingleProduct = async (req, res) => {
   const pool = await poolPromise;
   const result = await pool
     .request()
-    .query(`SELECT * FROM [dbo].[item] where item_id=${req.body.item_id}`);
+    .query(`SELECT * FROM [dbo].[item] where item_id=${req.body}`);
   res
     .status(200)
     .json({ data: result.recordsets[0], nbHits: result.recordsets[0].length });
